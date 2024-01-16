9fef77cb5f06acb1504606acb147c06dd3cbf8a2
Cherrypick14 <cherrypicklet@gmail.com>
2024-01-16 23:37:33 +0300

    Throwing a new error

---
+++ throwError.js
@@ -0,0 +1,8 @@
+// Throwing an error means we stop runnig a code art that point
+// We create an object instance using the new keyword
+
+function throwError() {
+    throw new Error("I got you Error")
+}
+
+module.exports = throwError;
