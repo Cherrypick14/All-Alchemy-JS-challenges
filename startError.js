c80b36c5c6240c17488855d5a6f72ee8d6fcc0a1
Cherrypick14 <cherrypicklet@gmail.com>
2024-01-18 22:38:17 +0300

    adding a type error in js

--- startError.js
+++ startError.js
@@ -3,3 +3,11 @@ function startError() {
    new Array(NaN)
 }
 module.exports= startError
+
+// adding a type error in Js
+function startError() {
+    const y = "kiddo"
+
+    y();
+ }
+ module.exports= startError
