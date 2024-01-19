1ecb7aad3fc97ffa03b8b55308084b7ad2d4951f
Cherrypick14 <cherrypicklet@gmail.com>
2024-01-19 23:38:08 +0300

    finished converting string to number implictly

---
+++ toNumber.js
@@ -0,0 +1,6 @@
+//The goal here is to convert the string to number
+// This can be done explictly or implictly so we are going the implictly way
+function toNumber(string){
+    const parsedNumber = +string
+    return isNaN(parsedNumber) ? 0 : parsedNumber
+}
